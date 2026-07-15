e3fcab02c48ac5e88cae0bbdf9198622826101f0
Cristian Reyes <cceyesmolina@gmail.com>
2026-07-14 23:40:21 -0500

    Use Anomaly Arbitration submissions

--- src/app/remote-runs.ts
+++ src/app/remote-runs.ts
@@ -2,7 +2,7 @@ import type { RawRun } from "../domain/types.js";
 import { fetchJson } from "./json-file.js";
 
 const RUNS_URL =
-  "https://theherta.com/api/archive/submissions?season=4.3&mode=Memory+of+Chaos&bossName=Murata+Graphia%2C+Founding+Artist";
+  "https://theherta.com/api/archive/submissions?season=4.4&mode=Anomaly+Arbitration&bossName=Murata+Graphia%2C+Founding+Artist";
 
 export function fetchRemoteRuns(): Promise<RawRun[]> {
   return fetchJson<RawRun[]>(RUNS_URL);
